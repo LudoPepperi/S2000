6024210be133cb01a193e7a84664d250cfba6280
LudoPepperi <77826528+LudoPepperi@users.noreply.github.com>
2024-08-14 15:09:13 +0200

    Update body.js

--- body.js
+++ body.js
@@ -20,7 +20,7 @@ var Brands = [
 //Promotions block
 var Promotions = [
     {
-        title: "N'hesitez pas à prendre contact",
+        title: "",
         buttonText: "Cliquez ici",
         link: 'Transactions/scope_items/{{UUID}}?SearchString=&CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%225332e389-e1f9-421e-bc33-689bdfbca16c%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%2299207e51-47b5-4f30-8271-c364a6882050%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0&SearchAll=false',
         image: 'https://ludopepperi.github.io/S2000/Offre1_1.jpg'
@@ -28,7 +28,7 @@ var Promotions = [
     ]
 var Promotions2 = [
     {
-        title: "Livraison Rapide",
+        title: "",
         buttonText: "Cliquez ici",
         link: 'Transactions/scope_items/{{UUID}}?SearchString=&CurrentTab=%22%7B%5C%22JsonFilter%5C%22:%5C%225332e389-e1f9-421e-bc33-689bdfbca16c%5C%22,%5C%22Parent%5C%22:%5C%22%7B%5C%5C%5C%22JsonFilter%5C%5C%5C%22:%5C%5C%5C%221fed1213-9371-4267-8e07-c83c56b057e1%5C%5C%5C%22%7D%5C%22%7D%22&TopPadding=0&SearchAll=false',
         image: 'https://ludopepperi.github.io/S2000/Offre1_2.jpg'
@@ -94,7 +94,7 @@ var CaruselData = [
 var CaruselDataSecond = [
     {
 
-        title: '',
+        title: "N'hesitez pas à prendre contact",
         imageURL: 'https://ludopepperi.github.io/S2000/Carousel2_1.jpg',
         description: '',
         buttonText: '',
@@ -103,7 +103,7 @@ var CaruselDataSecond = [
     },
     {
 
-        title: '',
+        title: "Livraison Rapide",
         imageURL: 'https://ludopepperi.github.io/S2000/Carousel2_2.jpg',
         description: '',
         buttonText: '',
